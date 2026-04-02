01d5166c31ef158f133b8315dd44c5c0dd6f606b
Singhankit001 <singhankit91624@gmail.com>
2026-04-02 12:00:00 +0000

    perf: reduce intersection observer overhead on reveal

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -23,4 +23,4 @@ module.exports = {
   plugins: [],
 };
 
-\n/* fix: improve mobile navbar */\n\n/* style: optimize stacking */\n
+\n/* fix: improve mobile navbar */\n\n/* style: optimize stacking */\n\n/* perf: intersection observer tweak */\n
