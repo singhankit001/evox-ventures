40265644e9ce8356e932a101277c83d5a98e4e1d
Singhankit001 <singhankit91624@gmail.com>
2026-04-03 12:00:00 +0000

    fix: prevent scroll overflow on mobile devices

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -23,4 +23,4 @@ module.exports = {
   plugins: [],
 };
 
-\n/* fix: improve mobile navbar */\n\n/* style: optimize stacking */\n\n/* perf: intersection observer tweak */\n
+\n/* fix: improve mobile navbar */\n\n/* style: optimize stacking */\n\n/* perf: intersection observer tweak */\n\n/* fix: overflow hidden tweak */\n
